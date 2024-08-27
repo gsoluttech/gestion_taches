f25e38d9b74f01c95219c71e0daf7813ef656bd1
PromesseMusay <promessemusay@gmail.com>
2024-08-27 16:44:34 +0200

    projt

--- resources/views/MainCordoView.php
+++ resources/views/MainCordoView.php
@@ -1,30 +1,34 @@
+<?php 
+  require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'projet.php';
+?>
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
 <div class="p-8">
   <!-- creation du projet -->
   <div class="mb-8">
     <h2 class="text-xl font-semibold mb-4">Créer un projet</h2>
-    <form class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <form class="grid grid-cols-1 md:grid-cols-2 gap-4" method="post">
       <div>
         <label class="block text-gray-700">Nom</label>
-        <input type="text" class="mt-1 p-2 block w-full border border-gray-300 rounded-md" />
+        <input type="text" class="mt-1 p-2 block w-full border border-gray-300 rounded-md" name="nomprojet"/>
       </div>
       <div>
         <label class="block text-gray-700">Description</label>
-        <textarea class="mt-1 p-2 block w-full border border-gray-300 rounded-md"></textarea>
+        <textarea class="mt-1 p-2 block w-full border border-gray-300 rounded-md" name="description"></textarea>
       </div>
       <div>
         <label class="block text-gray-700">Date début</label>
-        <input type="date" class="mt-1 p-2 block w-full border border-gray-300 rounded-md" />
+        <input type="date" class="mt-1 p-2 block w-full border border-gray-300 rounded-md" name="dateDebut" />
       </div>
       <div>
         <label class="block text-gray-700">Date fin</label>
-        <input type="date" class="mt-1 p-2 block w-full border border-gray-300 rounded-md" />
+        <input type="date" class="mt-1 p-2 block w-full border border-gray-300 rounded-md" name="dateFin"/>
       </div>
-    </form>
-    <button class="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
+      <button class="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700" type="submit" name="createproject">
       Créer
     </button>
+    </form>
+
   </div>
 
   <!-- creation de l'activite -->
@@ -83,3 +87,18 @@
     </button>
   </div>
 </div>
+
+
+<?php  
+
+if (isset($_POST["createproject"])) {
+  $nomProjet = $_POST["nomprojet"];
+  $description = $_POST["description"];
+  $dateDebut = $_POST["dateDebut"];
+  $dateFin = $_POST["dateFin"];
+
+  echo "Valeur aredsjkfhd";
+
+
+}
+?>
