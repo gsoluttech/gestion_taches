9a16f309218b3701bbb87eceeb550cbb9dde1328
PromesseMusay <promessemusay@gmail.com>
2024-08-28 13:47:32 +0200

    Main cordo view 2 update

--- resources/views/MainCordoView2.php
+++ resources/views/MainCordoView2.php
@@ -1,4 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/boxicons@2.1.1/css/boxicons.min.css" rel="stylesheet">
+
 
 <div class="bg-gray-50 min-h-screen flex items-center justify-center">
     <div class="bg-white w-full max-w-3xl rounded-lg shadow-lg p-8">
@@ -14,8 +16,20 @@
         <!-- Section du rapport récent -->
         <div>
             <h2 class="text-lg font-semibold text-gray-700 mb-4">Rapport récent</h2>
-            <div class="bg-gray-100 p-6 rounded-lg h-64">
-                <!-- Contenu du rapport récent -->
+            <div class="bg-gray-100 p-6 rounded-lg h-64 overflow-y-auto">
+                <!-- Template du rapport récent -->
+                <div class="flex items-center justify-between bg-white p-4 mb-4 rounded-lg shadow">
+                    <div class = "flex flex-row">
+                        <h3 class="text-md font-semibold text-gray-800">Nom du fichier</h3>
+                        <p class="text-sm text-gray-600 px-16">Ceci est une brève description du fichier.</p>
+                    </div>
+                    <a href="#" class="text-blue-600 hover:text-blue-800">
+                    <i class='bx bx-cloud-download text-4xl'></i>
+                    </a>
+                </div>
+                <!-- Fin du template du rapport récent -->
+
+                <!-- Vous pouvez dupliquer ce bloc pour chaque rapport -->
             </div>
         </div>
     </div>
